844b4b7c9031ffe2d96f675127a1b4de5b0a14f6
farzadoxo <farzadebrahimi.oxo@gmail.com>
2024-10-25 16:41:33 +0330

    🎨 js file added

---
+++ js/inedx.js
@@ -0,0 +1,4 @@
+function send()
+{
+    console.log("tets");
+}
---
+++ js/jquery.easing.1.3.js
@@ -0,0 +1,18 @@
+
+jQuery.easing['jswing'] = jQuery.easing['swing'];
+
+jQuery.extend( jQuery.easing,
+{
+	def: 'easeOutQuad',
+	swing: function (x, t, b, c, d) {
+		//alert(jQuery.easing.default);
+		return jQuery.easing[jQuery.easing.def](x, t, b, c, d);
+	},
+
+	easeInOutExpo: function (x, t, b, c, d) {
+		if (t==0) return b;
+		if (t==d) return b+c;
+		if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+		return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+	}
+});
